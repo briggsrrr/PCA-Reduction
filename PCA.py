014912090aaea1adcdd0b5f98cc31a075cd13a93
briggsrr <robert.briggs.3@icloud.com>
2022-07-05 18:28:03 -0700

    Integrated several functions, generalized original solution

--- PCA.py
+++ PCA.py
@@ -7,12 +7,14 @@ import numpy.linalg as npla
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import axes3d 
+from sklearn import datasets
 
 
 def convert(letter):
     return ord(letter) - 65
 
 def createScreePlot(C): 
+    '''C is the covariance matrix of the dataset'''
     U, sigma, Vt = npla.svd(C) 
     
     numList = []
@@ -27,7 +29,6 @@ def createScreePlot(C):
         numList.append(num)
         accumList.append(accum)
         
-    
     screeWindow = plt.figure()
     plt.ylim(0,100)
     ax = screeWindow.add_axes([0,0,1,1])
@@ -42,60 +43,99 @@ def createScreePlot(C):
     ax.bar(PC_count, numList)
     plt.plot(accumList, 'r-')
     screeWindow.show()
-    
 
+  
 
-   
+def getDataMatrix():
+    '''Gets user input and produces data matrix accordingly'''
 
-my_data = np.loadtxt(open("GeneralFormat12Col.csv", "rb"), delimiter=",", skiprows=1, converters={0: convert})  
+    userInput  = input("\nEnter a custom filename or enter 'iris' to use the sklearn iris dataset or 'breastcancer' to use the sklearn breast cancer dataset: ")
 
-D = my_data[:,1:9]
-y = my_data[:,0]
-(n, p) = D.shape
-print("Data set loaded: {} observations and {} columns\n".format(n, p))
-
-mu = np.mean( D, axis=0 )                     
-M = np.outer( np.ones( (n, 1) ), mu )         
-Z = (D - M)
-
-Z = Z / np.std(D, axis = 0)
-
-C = np.cov(Z.T, bias = False)
-
-#finding evals and evecs
-evals, evecs = npla.eig(C)
-
-#We need to sort evals and evecs since both are not presented in a sorted fashion
-#by numpy, so when comparing to sigma, we need to sort them in descending order
-
-sorted_evals = evals[evals.argsort()[::-1]].real
-sorted_evecs = evecs[:,evals.argsort()[::-1]]
-
-createScreePlot(C)
+    if userInput.lower() == "iris":
+        iris = datasets.load_iris()
+        D = iris.data     
+        y = iris.target  
 
 
 
-
-U, sigma, Vt = npla.svd(C)
-
-principalComponent0, principalComponent1 = U[:,0], U[:,1]
-
-x0 = Z @ principalComponent0
-x1 = Z @ principalComponent1
+    elif userInput.lower() == "breastcancer":
+        breast_cancer = datasets.load_breast_cancer()
+        D = breast_cancer.data     
+        y = breast_cancer.target 
 
 
-# Plotting projected data.
-fig = plt.figure(dpi=140)
-ax = fig.add_subplot( 111 )
+    else: 
+        try: 
+            in_data = np.loadtxt(open(userInput, "rb"), delimiter=",", skiprows=1, converters={0: convert}) 
+            in_data_cols = in_data.shape[1] 
+            D = in_data[:,1:in_data_cols]
+            (n, p) = D.shape
+            y = in_data[:,0]
+            print("Data set loaded: {} observations and {} columns\n".format(n, p))
+          
+        except: 
+            print("\nAn exception occurred, check spelling and try again (make sure to tag on .csv to the end of file).\n")
+        
+    return (D,y)
+        
+def getCovarianceMatrix(D,y,useNpCov = True):
+    '''Standardizes the data and produces the covariance matrix 
 
-ax.set_title( "Pizza Data Set Projected onto First Two Principal Components" )
+    IN: -D is the dataset data
+        -y is the dataset target
+        -the bool useNpCov decides whether or use np.cov or manually calculate the cov matrix. (true = np.cov)
 
-for i in range(10):
-    ax.plot( x0[y == i], x1[y == i],".", markersize=2, label="Type" + str(i))
+    OUT: -Returns the covariance matrix and the standardized vector Z
+    '''
+    n = D.shape[0]
+    mu = np.mean( D, axis=0 )                     
+    M = np.outer( np.ones( (n, 1) ), mu )         
+    Z = (D - M)
+    Z = Z / np.std(D, axis = 0)
 
-ax.set_xlabel( "First principal component" )
-ax.set_ylabel( "Second principal component" )
-plt.legend(loc='upper left')
-plt.ylim([-3,3])
-plt.show()
+    if useNpCov: 
+        C = np.cov(Z.T, bias = False) # use bias = true for small data sets
+        
+    else: 
+        C = (((D-M).T@(D-M))/(n))
+
+    return C,Z; 
+
+def performPCA(C, Z, y): 
+    U, sigma, Vt = npla.svd(C)
+
+    principalComponent0, principalComponent1 = U[:,0], U[:,1]
+
+    x0 = Z @ principalComponent0
+    x1 = Z @ principalComponent1
+
+    # Plotting projected data.
+    fig = plt.figure(dpi=140)
+    ax = fig.add_subplot( 111 )
+
+    ax.set_title( "Data Set Projected onto First Two Principal Components" )
+
+
+    for i in range(np.unique(y).size):
+        ax.plot( x0[y == i], x1[y == i],".", markersize=2, label="Type " + str(i))
+
+    ax.set_xlabel( "First principal component" )
+    ax.set_ylabel( "Second principal component" )
+    plt.legend(loc='upper left')
+    plt.ylim([-6,6])
+
+    plt.show()
+
+
+
+
+def main(): 
+    D,y = getDataMatrix()
+    C,Z = getCovarianceMatrix(D,y)
+    createScreePlot(C)
+    performPCA(C,Z,y)
+
+
+
+main() 
 
--- README.md
+++ README.md
@@ -2,22 +2,25 @@
 This project takes a dataset file and produces a screeplot for analyzing the PC components as well as the dataset projected on the first two PC components.
 
 
-Some file/folder descriptions: 
+Some file descriptions: 
  >  - DataGenerator.py: Can generate valid .cvs files for random based testing (Some are already generated)
- >  - GoodPCAData.csv is a non-random dataset, and is as the name implies
+ >  - Nutrition.csv is a non-random dataset that works well with PCA
  >  - PCA.py: Performs PCA dimentionality reduction on given dataset
 
      
 To use with an sklearn dataset (IRIS or BREASTCANCER): 
- > (incomplete)
+ > 1. python3 ./pca.py
+ > 2. enter either "iris" or "breastcancer" to use the respective sklearn datasets
 
 
 To use with a data file: 
-> (incomplete)
+> 1. python ./pca.py 
+> 2. enter the name of the file ex. "Nutrition.csv"
   
 Notes: 
 
-> Currently designed to only take data files configured in a certain way. To see configuration reference any of the .csv files in this project.  
+> Currently designed to only take input data files configured in a certain way. To see configuration reference any of the .csv files in this project.  
+> Make sure to have sklearn, matplotlib, and numpy libraries installed 
 
 
 
